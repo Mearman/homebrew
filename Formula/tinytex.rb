c9ddc04d3f0cbc2c20bad787440fc0d858de4631
Mearman <Mearman@users.noreply.github.com>
2024-06-02 00:01:09 +0000

    Update TinyTeX to v2024.06

--- Formula/tinytex.rb
+++ Formula/tinytex.rb
@@ -1,8 +1,8 @@
 class Tinytex < Formula
   desc "Tiny and easy-to-maintain LaTeX distribution based on TeX Live"
   homepage "https://yihui.org/tinytex/"
-  url "https://github.com/rstudio/tinytex-releases/releases/download/v2023.06/TinyTeX-1-v2023.06.tar.gz"
-  sha256 "f9d94f4787b5d6814409257d30665c1c9c72318c45051d5569f11563221f3c64"
+  url "https://github.com/rstudio/tinytex-releases/releases/download/v2024.06/TinyTeX-1-v2024.06.tar.gz"
+  sha256 "dc36dffa6f01ad5186d718d05d338e9c538ca056a1c419da72fded96240c20fc"
   license "GPL-2.0-or-later"
   head "https://yihui.org/tinytex/TinyTeX-1.tgz"
 
